3bf7d315e3c9bff48e77b325e8c826b94d9d6ec6
Cameron Diggs <ctd8z@virginia.edu>
2015-04-22 18:10:13 -0400

    Adding User model and links between pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/index'
+  get 'users/new'
 
-  get 'static_pages/calculate'
+  root 'static_pages#home'
+  get 'index' => 'static_pages#index'
+
+  get 'calculate' => 'static_pages#calculate'
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
